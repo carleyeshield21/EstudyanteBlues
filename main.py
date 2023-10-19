9f762f08f87e43498593db8c807f958b69bfdea0
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-19 15:45:22 +0800

    kumite

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ class AgeCalculator(QWidget):
         super().__init__() #To call the init of the parent class, in this code QWidget
         grid = QGridLayout()
 
-        self.name_label = QLabel('Name:') #name widget, adding self to make the variable accessible to the other functions, we also have to add the self to the addWidget placement
-        name_line_edit = QLineEdit()
+        name_label = QLabel('Name:') #name widget
+        self.name_line_edit = QLineEdit() #adding self to make the variable accessible to the other functions, we also have to add the self to the addWidget placement
 
         calculate_button = QPushButton('Calculate Age') #creating a button
         calculate_button.clicked.connect(self.calculate_age) #linking the button to the calculate_age function
@@ -18,8 +18,8 @@ class AgeCalculator(QWidget):
         self.date_birth_line_edit = QLineEdit() #adding self to make the variable accessible to the other functions, we also have to add the self
         # to the addWidget placement
 
-        grid.addWidget(self.name_label, 0, 0) #placement of widget
-        grid.addWidget(name_line_edit, 0, 1) #placement of widget
+        grid.addWidget(name_label, 0, 0) #placement of widget
+        grid.addWidget(self.name_line_edit, 0, 1) #placement of widget
         grid.addWidget(date_birth_label, 1, 0) #placement of widget
         grid.addWidget(self.date_birth_line_edit, 1, 1) #placement of widget
         grid.addWidget(calculate_button, 2, 0, 1, 2) #row2, column0, span of 1 row and 2 columns
@@ -30,8 +30,9 @@ class AgeCalculator(QWidget):
     def calculate_age(self):
         current_year = datetime.datetime.now() #retrieving the current year
         year_of_birth = self.date_birth_line_edit.text() #added the text method so we can extract the actual value
-        age = current_year - year_of_birth
-        self.output_age_label.setText()
+        birth_year = datetime.datetime.strptime(year_of_birth, "%m/%d/%Y").date().year
+        age = current_year - birth_year
+        self.output_age_label.setText(f"{self.name_line_edit.text()}'s age is {age} years")
 
 app = QApplication(sys.argv)
 age_calculator = AgeCalculator()
