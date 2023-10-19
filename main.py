3971d60db3ba4bf124f7daa5f4ab569c796b8314
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-19 13:57:55 +0800

    kumite

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ class AgeCalculator(QWidget):
         name_label = QLabel('Name:')
         name_line_edit = QLineEdit()
 
-        name_label = QLabel('Date of birth MM/DD/YYYY')
+        date_birth_label = QLabel('Date of birth MM/DD/YYYY')
         date_birth_line_edit = QLineEdit()
 
+        grid.addWidget(name_label, 0, 0)
+        grid.addWidget(name_line_edit, 0, 1)
+        grid.addWidget(date_birth_label, 1, 0)
+        grid.addWidget(date_birth_line_edit, 1, 1)
 
 
 
