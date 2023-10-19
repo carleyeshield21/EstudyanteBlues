fa59d9139603aa256410b7cff88c0ed19dcd8a78
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-19 13:51:39 +0800

    kumite

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget,               QGridLayout, QLineEdit
+
+class AgeCalculator(QWidget):
+    def __init__(self):
+        grid = QGridLayout()
+
+        name_label = QLabel('Name:')
+        name_line_edit = QLineEdit()
+
+        name_label = QLabel('Date of birth MM/DD/YYYY')
+        date_birth_line_edit = QLineEdit()
+
+
+
+
+if __name__ == '__main__':
+    AgeCalculator()
