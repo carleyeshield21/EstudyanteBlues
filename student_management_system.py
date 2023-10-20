067be4c84f4fdce1e2cf5f1012b82dd522a2a1e7
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-20 12:16:53 +0800

    kumite

--- student_management_system.py
+++ student_management_system.py
@@ -5,7 +5,11 @@ import datetime
 class MainWindow(QMainWindow): #QMainWindow has a menu bar, toolbar, status bar
     def __init__(self):
         super().__init__() #To call the init of the parent class, in this code QMainWindow
-        self.setWindowTitle('Estudyante Blues Management System')
+        self.setWindowTitle('Estudyante Blues Management System') #setting the title of the app
+
+        # Adding items on the menu bar
+        file_menu_item = self.menuBar().addMenu('&File') #Adding the ampersand sign in the beginning of the word will underscore the first letter
+        help_menu_item = self.menuBar().addMenu('&Help')
 
 app = QApplication(sys.argv)
 estudyante_blues = MainWindow()
