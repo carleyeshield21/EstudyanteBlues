906caca87e23d3d9abf73a323b5759df134a20ab
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-20 10:10:30 +0800

    kumite

--- student_management_system.py
+++ student_management_system.py
@@ -1,3 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget,  QGridLayout, QLineEdit, QPushButton
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget,  QGridLayout, QLineEdit, QPushButton, QMainWindow
 import sys
 import datetime
+
+class MainWindow(QMainWindow):
