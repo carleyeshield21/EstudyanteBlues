a22d946b9ea3c3c65cdd30d1777f7b29c746add3
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-20 12:46:57 +0800

    kumite

--- student_management_system.py
+++ student_management_system.py
@@ -24,6 +24,7 @@ class MainWindow(QMainWindow): #QMainWindow has a menu bar, toolbar, status bar
         # adding a table to the app, QTableWidget should be imported
         self.teybol = QTableWidget() #this variable will be accessed from another function to load the the table so we need to add the self keyword
         self.teybol.setColumnCount(20) #setting the column count of our table
+        self.teybol.setHorizontalHeaderLabels(('ID', 'Name', 'Course', 'MobileNumber')) #setting the label names of each column
 
     #creating another function for the table
     def load_data(self):
