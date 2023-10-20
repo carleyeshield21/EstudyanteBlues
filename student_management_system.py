adc6b62a7be58233a9c1e3d7124da94066b8f08c
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-20 13:16:57 +0800

    kumite

--- student_management_system.py
+++ student_management_system.py
@@ -29,6 +29,9 @@ class MainWindow(QMainWindow): #QMainWindow has a menu bar, toolbar, status bar
 
     #creating another function for the table
     def load_data(self):
+        kuneksyon = sqlite3.connect('database.db') #creating a connection to the database file
+        resulta_ng_database_teybol = kuneksyon.execute('SELECT * FROM students') #executing the connection from the database and making a database
+        # query
         self.teybol
 
 # the codes below are for the app to launch
