e5093969eaf59f728490b57a1d12d86335388ff5
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-20 13:34:25 +0800

    kumite

--- student_management_system.py
+++ student_management_system.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget,  QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget,  QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
 import sqlite3
@@ -32,8 +32,10 @@ class MainWindow(QMainWindow): #QMainWindow has a menu bar, toolbar, status bar
         kuneksyon = sqlite3.connect('database.db') #creating a connection to the database file
         resulta_ng_database_teybol = kuneksyon.execute('SELECT * FROM students') #executing the connection from the database and making a database
         # query
-        print(resulta_ng_database_teybol)
-        self.teybol
+        for index_row_number, row_data in enumerate(resulta_ng_database_teybol):
+            self.teybol.insertRow(index_row_number)
+            for index_column_number, data in enumerate(row_data):
+                self.teybol.setItem(index_row_number, index_column_number, QTableWidgetItem)
 
 # the codes below are for the app to launch
 app = QApplication(sys.argv)
