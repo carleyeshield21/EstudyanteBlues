2aaac7147ca52dac5d3c69382c9f121a463714f4
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-21 09:40:47 +0800

    kumite

--- inserting_new_records.py
+++ inserting_new_records.py
@@ -15,6 +15,7 @@ class MainWindow(QMainWindow): #QMainWindow has a menu bar, toolbar, status bar
         # adding the sub menus or the action to the items in the main menu
         add_student_action = QAction('Add Stoodent',self) #must from PyQt6.QtGui import QAction, must also add the argument self to show from the
         # drop down menu
+        add_student_action.triggered.connect(self.insert) #the function insert should be created
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction('About',self)
@@ -39,7 +40,7 @@ class MainWindow(QMainWindow): #QMainWindow has a menu bar, toolbar, status bar
             self.teybol.insertRow(index_row_number)
             for index_column_number, data in enumerate(row_data):
                 self.teybol.setItem(index_row_number, index_column_number, QTableWidgetItem(str(data)))
-        # kuneksyon.close()
+        kuneksyon.close()
 
 # the codes below are for the app to launch
 app = QApplication(sys.argv)
