1fefb2e11fb9a90ddb08ead083152e23403d495e
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-23 15:08:59 +0800

    kumite

--- edit_dialog.py
+++ edit_dialog.py
@@ -122,6 +122,7 @@ class EditDialog(QDialog):
 
         #get student name from selected row
         index = main_window.teybol.currentRow()
+        print(index)
         student_name = main_window.teybol.item(index,1).text() #1 is the Name column in the teybol, then apply the text() method to extract the text
         print(type(student_name))
 
