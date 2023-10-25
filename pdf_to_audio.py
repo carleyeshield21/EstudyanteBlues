d52d4f6f4e750d42e926b5893c3a098444eba59e
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-25 16:40:24 +0800

    kumite

--- pdf_to_audio.py
+++ pdf_to_audio.py
@@ -10,7 +10,11 @@ with open("the Real book of Real estate - robert kiyosaki.pdf", 'rb') as pdf:
         pdf_text.append(content)
 
 pdf_text_to_string = str(pdf_text)
-print(pdf_text_to_string)
+
+text_file = open('raw_pdf_file.txt', 'w')
+text_file.writelines(pdf_text_to_string)
+
+
 
 speaker = pyttsx3.init()
 speaker.say('stop look and listen')
