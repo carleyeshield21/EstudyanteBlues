490eca1ec03c4179c9efd81c7bee72ad0c483e8f
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-25 16:57:01 +0800

    kumite

--- pdf_to_audio.py
+++ pdf_to_audio.py
@@ -5,9 +5,12 @@ with open("the Real book of Real estate - robert kiyosaki.pdf", 'rb') as pdf:
     reader = PyPDF2.PdfReader(pdf, strict=False)
     pdf_text = []
 
+    pages = 0
     for page in reader.pages:
         content = page.extract_text()
         pdf_text.append(content)
+        pages += 1
+print(pages)
 
 pdf_text_to_string = str(pdf_text)
 
