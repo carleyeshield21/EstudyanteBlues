676c8adcb50cb82259ae81259857f78660773b1f
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-26 11:52:17 +0800

    kumite

--- pdf_to_audio.py
+++ pdf_to_audio.py
@@ -1,26 +1,40 @@
-import pyttsx3
-import PyPDF2
+# import pyttsx3
+# import PyPDF2
+#
+# with open("the Real book of Real estate - robert kiyosaki.pdf", 'rb') as pdf:
+#     reader = PyPDF2.PdfReader(pdf, strict=False)
+#     pdf_text = []
+#
+#     pages = 0
+#     for page in reader.pages:
+#         content = page.extract_text()
+#         pdf_text.append(content)
+#         pages += 1
+# print(pages)
+#
+# pdf_text_to_string = str(pdf_text)
+#
+# text_file = open('raw_pdf_file.txt', 'w')
+# text_file.writelines(pdf_text_to_string)
+#
+# speaker = pyttsx3.init()
+# speaker.say('How are you doing?')
+# speaker.runAndWait()
 
-with open("the Real book of Real estate - robert kiyosaki.pdf", 'rb') as pdf:
-    reader = PyPDF2.PdfReader(pdf, strict=False)
-    pdf_text = []
+import re
 
-    pages = 0
-    for page in reader.pages:
-        content = page.extract_text()
-        pdf_text.append(content)
-        pages += 1
-print(pages)
+text = "Hello, my email is example@email.com and my phone number is 123-456-7890."
 
-# pages = PyPDF2.PdfReader.getPage()
-# text = pages.extractText()
+# Find an email address
+email_pattern = r'\S+@\S+'
+email = re.search(email_pattern, text)
+print("Email:", email.group() if email else "Not found")
 
-pdf_text_to_string = str(pdf_text)
-
-text_file = open('raw_pdf_file.txt', 'w')
-text_file.writelines(pdf_text_to_string)
-
-
-speaker = pyttsx3.init()
-speaker.say('How are you doing?')
-speaker.runAndWait()
+# Find all phone numbers
+# phone_pattern = r'\d{3}-\d{3}-\d{4}'
+# phones = re.findall(phone_pattern, text)
+# print("Phone Numbers:", phones)
+#
+# # Replace phone numbers with "REDACTED"
+# redacted_text = re.sub(phone_pattern, "REDACTED", text)
+# print("Redacted Text:", redacted_text)
--- scratch.py
+++ scratch.py
@@ -1,21 +1,32 @@
-from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QLabel, QWidget,  QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget,
-                             QTableWidgetItem, QDialog, QComboBox, QToolBar, QStatusBar)
-import sys
-import sqlite3
-def on_combobox_selection(index):
-    selected_text = my_combo_box.itemText(index)
-    print(f"Selected item: {selected_text}")
+import re
 
-# course_name = main_window.teybol.item(index,2).text()
-app = QApplication(sys.argv)
-window = QMainWindow()
-window.setGeometry(100, 100, 400, 200)
+file = open('sample_text.txt')
+txt = str(file.readlines())
+print(type(txt))
+# my_list = ["apple", "banana", "cherry"]
+# my_string = "".join(txt)
+# print(my_string)
 
-my_combo_box = QComboBox(window)
-my_combo_box.addItem("Option 1")
-my_combo_box.addItem("Option 2")
-my_combo_box.addItem("Option 3")
-my_combo_box.currentIndexChanged.connect(on_combobox_selection)
+# text = "TheREAL Book \nof \nReal Estate9781593155322_FM:real estate_new  3/25/09  3:52 PM  Page i', '9781593155322_FM:real estate_new  3/25/09  3:52 PM  Page ii\nThis page intentionally left blank ', 'The REAL Book \nof \nREAL EXPERTS. REAL STORIES. REAL LIFE.\nRobert Kiyosaki\nReal Estate9781593155322_FM:real estate_new  3/25/09  3:52 PM  Page iii', 'Copyright © 2009 by Robert T. Kiyosaki\nPublished by Vanguard PressAll rights reserved. No part of this publication may be reproduced, stored in a retrieval \nsystem"
+# print(type(text))
 
-window.show()
-sys.exit(app.exec())
+# Input text containing newline characters
+# text = "This is a\nsample text\nwith newline characters."
+
+# Define a regex pattern to match newline characters
+# pattern = r'\n'
+pattern = r'\\'
+
+# Replace newline characters with a space
+# result = re.sub(pattern, ' ', text)
+result = re.sub(pattern, '', txt)
+
+# Print the result
+print(result)
+print(type(result))
+
+
+# result = re.sub(pattern, ' ', text)
+#
+# # Print the result
+# print(result)
