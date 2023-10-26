0f3ec7c7ebde4dfa22d55ed45b6051f595fc1b5a
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-26 12:26:09 +0800

    kumite

--- regex_sample.py
+++ regex_sample.py
@@ -2,22 +2,37 @@ import re
 
 file = open('sample_text.txt')
 txt = str(file.readlines())
-print(type(txt))
+# print(type(txt))
+# print(txt)
+
+pattern = r'\\n' #creating a pattern to detect the breakline \n but this will still leave a single backslash to the output text
+text1 = re.sub(pattern,'',txt) #replacing the breakline with a single space, a single backslash will still remain in the output text
+txt = text1
+print(txt)
 
 # text = "TheREAL Book \nof \nReal Estate9781593155322_FM:real estate_new  3/25/09  3:52 PM  Page i', '9781593155322_FM:real estate_new  3/25/09  3:52 PM  Page ii\nThis page intentionally left blank ', 'The REAL Book \nof \nREAL EXPERTS. REAL STORIES. REAL LIFE.\nRobert Kiyosaki\nReal Estate9781593155322_FM:real estate_new  3/25/09  3:52 PM  Page iii', 'Copyright © 2009 by Robert T. Kiyosaki\nPublished by Vanguard PressAll rights reserved. No part of this publication may be reproduced, stored in a retrieval \nsystem"
 # print(type(text))
 
+
+
+# text = "TheREAL Book \of \Real Estate9781593155322_FM:real estate_new  3/25/09  3:52 PM  Page i'"
+# patt = r'\\'
+# res = re.sub(patt,'',text)
+# text = res
+# print(text)
 # Input text containing newline characters
 # text = "This is a\nsample text\nwith newline characters."
 
 # Define a regex pattern to match newline characters
 # pattern = r'\n'
-pattern = r'\\'
-
-# Replace newline characters with a space
-# result = re.sub(pattern, ' ', text)
-result = re.sub(pattern, '', txt)
-
-# Print the result
-print(result)
-print(type(result))
+# pattern = r'\\n'
+# x = re.compile(pattern)
+# print(x)
+#
+# # Replace newline characters with a space
+# # result = re.sub(pattern, ' ', text)
+# result = re.sub(pattern, '', txt)
+#
+# # Print the result
+# print(result)
+# print(type(result))
